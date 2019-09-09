12b434f92b3e5839773cf6e1bf0bed8b78cdf2d6
Prabha97 <prabhashwari.lk@gmail.com>
2019-09-09 17:27:49 +0530

    Compleated show, update, delete operations in fireBase

--- app/src/main/java/com/example/testapplicationfirebase/MainActivity.java
+++ app/src/main/java/com/example/testapplicationfirebase/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.testapplicationfirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,8 +10,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -59,16 +63,102 @@ public class MainActivity extends AppCompatActivity {
                     }else if(TextUtils.isEmpty(txtContact.getText().toString())) {
                         Toast.makeText(getApplicationContext(), "Please Enter Your Contact Number.", Toast.LENGTH_SHORT).show();
                     }else{
-                        std.setId(txtId.getText().toString());
-                        std.setName(txtName.getText().toString());
-                        std.setAddress(txtAddress.getText().toString());
-                        std.setContactNo(Integer.parseInt(txtContact.getText().toString()));
+                        std.setId(txtId.getText().toString().trim());
+                        std.setName(txtName.getText().toString().trim());
+                        std.setAddress(txtAddress.getText().toString().trim());
+                        std.setContactNo(Integer.parseInt(txtContact.getText().toString().trim()));
 
                         dbRef.push().setValue(std);
                     }
                 }catch(NumberFormatException e){
                     Toast.makeText(getApplicationContext(),"Invalide Contact Number.Please Try again.",Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        btnshow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Student").child("-Lnw9UVF0g-MTf3xnf0r");
+                ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.hasChildren()){
+                            txtId.setText(dataSnapshot.child("id").getValue().toString());
+                            txtName.setText(dataSnapshot.child("name").getValue().toString());
+                            txtAddress.setText(dataSnapshot.child("address").getValue().toString());
+                            txtContact.setText(dataSnapshot.child("contactNo").getValue().toString());
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(),"No Source to Display.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+        btnupdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseReference updateref = FirebaseDatabase.getInstance().getReference().child("Student");
+                updateref.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.hasChild("-Lnw9UVF0g-MTf3xnf0r")){
+                            try{
+                                std.setId(txtId.getText().toString().trim());
+                                std.setName(txtName.getText().toString().trim());
+                                std.setAddress(txtAddress.getText().toString().trim());
+                                std.setContactNo(Integer.parseInt(txtContact.getText().toString().trim()));
+
+                                dbRef = FirebaseDatabase.getInstance().getReference().child("Student").child("-Lnw9UVF0g-MTf3xnf0r");
+                                dbRef.setValue(std);
+                                clearControls();
+
+                                Toast.makeText(getApplicationContext(),"Data Updated Successfully.",Toast.LENGTH_SHORT).show();
+                            }catch(NumberFormatException e){
+                                Toast.makeText(getApplicationContext(), "Invalid Contact Number.", Toast.LENGTH_SHORT).show();
+
+                            }
+                        }else
+                            Toast.makeText(getApplicationContext(), "No source to Update.", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+        btndelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseReference deleteRef = FirebaseDatabase.getInstance().getReference().child("Student");
+                deleteRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.hasChild("-Lnw9UVF0g-MTf3xnf0r")){
+                            dbRef = FirebaseDatabase.getInstance().getReference().child("Student").child("-Lnw9UVF0g-MTf3xnf0r");
+                            dbRef.removeValue();
+                            clearControls();
+                            Toast.makeText(getApplicationContext(),"Data Deleted Successfully.",Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(getApplicationContext(),"No such a source to Delete.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
             }
         });
     }
